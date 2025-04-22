38ac3a302912462c82f98816de3ce6b42d0d90c3
davidvchang <dvalenzuelachang@gmail.com>
2025-04-21 23:55:16 -0700

    Fix: background color in categories

--- frontend/src/components/Category.tsx
+++ frontend/src/components/Category.tsx
@@ -9,11 +9,11 @@ const Category:React.FC<PropsCategory> = ({text, isSelected}) => {
   return (
     <>
         {isSelected ? (
-            <button className='flex items-center justify-center w-fit h-fit px-3 py-0.5 border border-blue-600 rounded-full bg-blue-600'>
+            <button className='flex items-center justify-center w-fit h-fit px-3 py-0.5 border border-blue-600 rounded-full bg-blue-600 cursor-pointer'>
                 <span className='text-sm text-white'>{text}</span>
             </button>
         ) : (
-            <button className='flex items-center justify-center w-fit h-fit px-3 py-0.5 border border-blue-300 rounded-full bg-blue-200'>
+            <button className='flex items-center justify-center w-fit h-fit px-3 py-0.5 border border-blue-200 rounded-full bg-blue-100 cursor-pointer'>
                 <span className='text-sm text-sky-900'>{text}</span>
             </button>
         )}
