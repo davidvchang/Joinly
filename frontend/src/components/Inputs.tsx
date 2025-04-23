cbb7fd235200edb80cb6c3ea5dd610150b505231
davidvchang <dvalenzuelachang@gmail.com>
2025-04-23 14:42:43 -0700

    Add: CreateEvent page design

--- frontend/src/components/Inputs.tsx
+++ frontend/src/components/Inputs.tsx
@@ -1,24 +1,37 @@
-import { IconNode } from 'lucide-react'
 import React, { ReactNode } from 'react'
 
 type PropsInputs = {
     text_label: string,
     htmlFor_label: string,
-    input_type: string,
+    input_type?: string,
     placeholder?: string,
-    icon: ReactNode
+    icon: ReactNode,
+    isTextArea?: boolean
 }
 
-const Inputs:React.FC<PropsInputs> = ({ text_label, htmlFor_label, input_type, placeholder, icon}) => {
+const Inputs:React.FC<PropsInputs> = ({ text_label, htmlFor_label, input_type, placeholder, icon, isTextArea}) => {
   return (
-    <div className='flex flex-col gap-1 w-full'>
-        <label htmlFor={htmlFor_label} className='text-sm'>{text_label}</label>
+    <>
+      {isTextArea ? (
+        <div className='flex flex-col gap-1 w-full'>
+            <label htmlFor={htmlFor_label} className='text-sm'>{text_label}</label>
 
-        <div className='relative w-full'>
-            <input type={input_type} name={htmlFor_label} id={htmlFor_label} className='border border-slate-300 rounded-md px-10 py-1.5 w-full placeholder:text-sm' placeholder={placeholder}/>
-            {icon}
+            <div className='relative w-full'>
+                <textarea name={htmlFor_label} id={htmlFor_label} className='border border-slate-300 rounded-md px-10 py-1.5 w-full min-h-32 placeholder:text-sm' placeholder={placeholder} required/>
+                {icon}
+            </div>
         </div>
-    </div>
+      ) : (
+        <div className='flex flex-col gap-1 w-full'>
+            <label htmlFor={htmlFor_label} className='text-sm'>{text_label}</label>
+
+            <div className='relative w-full'>
+                <input type={input_type} name={htmlFor_label} id={htmlFor_label} className='border border-slate-300 rounded-md pl-10 pr-2 py-1.5 w-full placeholder:text-sm' placeholder={placeholder} required/>
+                {icon}
+            </div>
+        </div>
+      )}
+    </>
   )
 }
 
--- frontend/src/pages/CreateEvent.tsx
+++ frontend/src/pages/CreateEvent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'lucide-react'
+import { Link, Tag, FileSpreadsheet, Calendar, Clock, MapPin } from 'lucide-react'
 import Inputs from '../components/Inputs'
 
 const CreateEvent:React.FC = () => {
@@ -12,17 +12,43 @@ const CreateEvent:React.FC = () => {
                     <span className='text-slate-600'>Fill in the details below to create your even</span>
                 </div>
 
-                <div className='flex flex-col gap-2'>
+                <form className='flex flex-col gap-2'>
                     <div className='flex flex-col gap-4'>
                         <div className='w-full h-60 bg-slate-300 rounded-lg overflow-hidden'>
                             <img src="https://kzmkmphxt6yi20wngsby.lite.vusercontent.net/placeholder.svg?height=200&width=400&text=Web+Dev+Workshop" alt="" className='w-full h-full object-cover'/>
                         </div>
 
                         <Inputs text_label='Image URL' htmlFor_label='image_url' input_type='text' icon={<Link className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='https://website.com/image'/>
+                        <Inputs text_label='Event Title' htmlFor_label='title' input_type='text' icon={<Tag className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='Give your event a name'/>
+                        <Inputs isTextArea={true} text_label='Description' htmlFor_label='description' icon={<FileSpreadsheet className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='Give your event a description'/>
+                        
+                        <div className='flex w-full items-center gap-5'>
+                            <Inputs text_label='Date' htmlFor_label='date' input_type='date' icon={<Calendar className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>}/>
+                            <Inputs text_label='Time' htmlFor_label='time' input_type='time' icon={<Clock className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>}/>
+                        </div>
+
+                        <Inputs text_label='Location' htmlFor_label='description' input_type='text' icon={<MapPin className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='Where will the event be held?'/>
+                    
+                        <div className="mb-4">
+                            <label htmlFor="category" className="text-sm">Category</label>
+                            <select id="category" name="category" className="rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-gray-900">
+                                <div className='flex flex-col bg-slate-200'>
+                                    <option value="">Sport</option>
+                                    <option value="">Technology</option>
+                                    <option value="">Development</option>
+                                    <option value="">Cook</option>
+
+                                </div>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className='flex items-center justify-end pt-3'>
+                        <button type="submit" className='w-fit h-fit px-5 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 hover:transition duration-300 cursor-pointer'>Create Event</button>
 
                     </div>
                     
-                </div>
+                </form>
             </div>
         </div>
     </section>
