0dbc60a7e81feae984f015c4aa996c4f1a6e7f9d
davidvchang <dvalenzuelachang@gmail.com>
2025-04-24 16:14:02 -0700

    Add: Endpoint getUsers and postUser

---
+++ backend/src/controllers/users.controller.js
@@ -0,0 +1,29 @@
+import {pool} from '../bd.js'
+import bcrypt from 'bcryptjs'
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await pool.query("SELECT * FROM users")
+        res.status(200).json(users.rows)
+    } catch (ex) {
+        res.status(500).json({message: "An error has ocurred to get all users", error: ex.message})
+    }
+}
+
+export const postUser = async (req, res) => {
+    const {image_url, name, last_name, email, password, phone_number} = req.body
+
+    try {
+        const existEmail = await pool.query("SELECT COUNT(*) FROM users WHERE email = $1", [email])
+        if(existEmail.rows[0].count > 0){
+            return res.status(409).json({message: "The email already exist"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        await pool.query("INSERT INTO users (image_url, name, last_name, email, password, phone_number) VALUES ($1, $2, $3, $4, $5, $6)", [image_url, name, last_name, email, hashedPassword, phone_number])
+        res.status(201).json({message: "User registered correctly"})
+    } catch (ex) {
+        res.status(500).json({message: "An error has ocurred to register the user", error: ex.message})
+    }
+}
--- backend/src/routes/users.route.js
+++ backend/src/routes/users.route.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
+import {getUsers, postUser} from '../controllers/users.controller.js'
 
 const router = Router()
 
-router.get('/')
-router.post('/')
+router.get('/', getUsers)
+router.post('/', postUser)
 
 router.get('/:id_user')
 router.delete('/:id_user')
