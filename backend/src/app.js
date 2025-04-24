76a31980c01d61d906958005bf8fba4b2411d3ee
davidvchang <dvalenzuelachang@gmail.com>
2025-04-24 16:41:32 -0700

    Add: cors configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import usersRoutes from './routes/users.route.js'
 
 const app = express()
@@ -7,6 +8,10 @@ const app = express()
 app.set('port', process.env.PORT || 4000)
 
 app.use(morgan("dev"))
+app.use(cors({
+    origin: '*',
+    credentials: true
+}))
 app.use(express.json())
 
 app.use('/api/users', usersRoutes)
