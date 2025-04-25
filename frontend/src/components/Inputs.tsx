f6050d281511184e43d553116b2ccfa63afbc442
davidvchang <dvalenzuelachang@gmail.com>
2025-04-24 21:18:05 -0700

    Add: validation inputs in register page

--- frontend/src/components/Inputs.tsx
+++ frontend/src/components/Inputs.tsx
@@ -7,9 +7,11 @@ type PropsInputs = {
     placeholder?: string,
     icon: ReactNode,
     isTextArea?: boolean
+    value: string,
+    onchange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
 }
 
-const Inputs:React.FC<PropsInputs> = ({ text_label, htmlFor_label, input_type, placeholder, icon, isTextArea}) => {
+const Inputs:React.FC<PropsInputs> = ({ text_label, htmlFor_label, input_type, placeholder, icon, isTextArea, value, onchange}) => {
   return (
     <>
       {isTextArea ? (
@@ -17,7 +19,7 @@ const Inputs:React.FC<PropsInputs> = ({ text_label, htmlFor_label, input_type, p
             <label htmlFor={htmlFor_label} className='text-sm'>{text_label}</label>
 
             <div className='relative w-full'>
-                <textarea name={htmlFor_label} id={htmlFor_label} className='border border-slate-300 rounded-md px-10 py-1.5 w-full min-h-32 placeholder:text-sm' placeholder={placeholder} required/>
+                <textarea name={htmlFor_label} value={value} onChange={onchange} id={htmlFor_label} className='border border-slate-300 rounded-md px-10 py-1.5 w-full min-h-32 placeholder:text-sm' placeholder={placeholder} required/>
                 {icon}
             </div>
         </div>
@@ -26,7 +28,7 @@ const Inputs:React.FC<PropsInputs> = ({ text_label, htmlFor_label, input_type, p
             <label htmlFor={htmlFor_label} className='text-sm'>{text_label}</label>
 
             <div className='relative w-full'>
-                <input type={input_type} name={htmlFor_label} id={htmlFor_label} className='border border-slate-300 rounded-md pl-10 pr-2 py-1.5 w-full placeholder:text-sm' placeholder={placeholder} required/>
+                <input type={input_type} value={value} onChange={onchange} name={htmlFor_label} id={htmlFor_label} className='border border-slate-300 rounded-md pl-10 pr-2 py-1.5 w-full placeholder:text-sm' placeholder={placeholder} required/>
                 {icon}
             </div>
         </div>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,10 +1,104 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CalendarIcon } from '@heroicons/react/24/solid'
 import { EnvelopeIcon, LockClosedIcon, UserIcon } from '@heroicons/react/24/outline'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Inputs from '../components/Inputs'
+import Swal from 'sweetalert2'
 
-const Register:React.FC = () => {
+import type {Users} from '../types/interfaces'
+import {registerUser, getAllUsers} from '../services/usersServices'
+
+interface ExtendUsers extends Users{
+  password: string,
+  confirm_password: string,
+}
+
+const Register:React.FC<Users> = () => {
+
+  const navigate = useNavigate()
+
+  const initialValues = {
+    id_user: 0,
+    image_url: '', 
+    name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+    phone_number: '',
+    joined_time: '', 
+  }
+
+  const [usersData, setUsersData] = useState<ExtendUsers>(initialValues)
+  const [errorMessage, setErrorMessage] = useState<string>("")
+  const [error, setError] = useState<boolean>(false)
+  const [differentPassword, setDifferentPassword] = useState<boolean>(false)
+  const [messageEightCharacters, setMessageEightCharacters] = useState<boolean>(false);
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const samePassword = verifyPassword()
+    const passwordCharacters = isPasswordTooShort()
+
+    if(!samePassword){
+      setDifferentPassword(true)
+      setTimeout(() =>setDifferentPassword(false), 2800)
+      return 
+    }
+
+    if(passwordCharacters) {
+      setMessageEightCharacters(true)
+      setTimeout(() =>setMessageEightCharacters(false), 2800)
+      return 
+    }
+
+    try {
+      const res = await registerUser(usersData)
+      
+      if(res.response.status === 201){
+        Swal.fire({
+          title: 'Registered User',
+          text: 'The user has been successfully registered',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        }).then((result) => {
+          if(result.isConfirmed){
+            navigate("/login")
+          }
+        })
+      }
+    } catch (ex: any) {
+      if(ex.response.status === 409){
+        setErrorMessage(ex.response.data.message)
+        setError(true)
+        setTimeout(() =>setError(false), 2800)
+      } else {
+        Swal.fire({
+          title: 'Error',
+          text: 'There was a problem registering the user.',
+          icon: 'error',
+          confirmButtonText: 'Try Again'
+        });
+      }
+    }
+  }
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setUsersData({ ...usersData, [e.target.name]: e.target.value })
+  }
+
+  const verifyPassword = () => {
+    if(usersData.password === usersData.confirm_password){
+      return true
+    }
+  }
+  
+  const isPasswordTooShort = () => {
+    return usersData.password.length < 8
+  }
+
   return (
     <section className='flex w-full' style={{height: "calc(100vh - 64px)"}}>
       <div className='w-full flex flex-col justify-center'>
@@ -19,15 +113,34 @@ const Register:React.FC = () => {
                 </div>
             </div>
 
-            <form className='flex flex-col gap-5'>
+            <form className='flex flex-col gap-5' onSubmit={handleSubmit}>
                 <div className='flex gap-2'>
-                    <Inputs text_label='Name' htmlFor_label='name' input_type='text' icon={<UserIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='Enter your name'/>
-                    <Inputs text_label='Last Name' htmlFor_label='last_name' input_type='text' icon={<UserIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='Enter your last name'/>
+                    <Inputs text_label='Name' value={usersData.name} onchange={handleOnChange} htmlFor_label='name' input_type='text' icon={<UserIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='Enter your name'/>
+                    <Inputs text_label='Last Name' value={usersData.last_name} onchange={handleOnChange} htmlFor_label='last_name' input_type='text' icon={<UserIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='Enter your last name'/>
                 </div>
 
-                <Inputs text_label='Email Address' htmlFor_label='email' input_type='email' icon={<EnvelopeIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='your@email.com'/>
-                <Inputs text_label='Password' htmlFor_label='password' input_type='password' icon={<LockClosedIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='•••••••••••'/>
-                <Inputs text_label='Confirm Password' htmlFor_label='confirm_password' input_type='password' icon={<LockClosedIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='•••••••••••'/>
+                <div className='flex flex-col'>
+                  <Inputs text_label='Email Address' value={usersData.email} onchange={handleOnChange} htmlFor_label='email' input_type='email' icon={<EnvelopeIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='your@email.com'/>
+                  {error  && (
+                    <span className='text-red-600 text-sm font-medium px-1 pt-1'>{errorMessage}</span>
+                  )}
+                </div>
+
+                <div className='flex flex-col'>
+                  <Inputs text_label='Password' value={usersData.password} onchange={handleOnChange} htmlFor_label='password' input_type='password' icon={<LockClosedIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='•••••••••••'/>
+                  
+                  {messageEightCharacters === true && (
+                    <span className='text-red-600 text-sm font-medium px-1 pt-1'>The password must be 8 characters long</span>
+                  )}
+                </div>
+                
+                <div className='flex flex-col'>
+                  <Inputs text_label='Confirm Password' value={usersData.confirm_password} onchange={handleOnChange} htmlFor_label='confirm_password' input_type='password' icon={<LockClosedIcon className='w-4 h-4 pointer-events-none absolute top-2.5 left-3 text-slate-500'/>} placeholder='•••••••••••'/>
+                  
+                  {differentPassword && (
+                    <span className='text-red-600 text-sm font-medium px-1 pt-1'>Password do not match</span>
+                  )}
+                </div>
 
                 <div className='flex flex-col gap-2'>
                     <button type='submit' className='bg-blue-500 h-fit py-2 text-white rounded-md font-medium hover:bg-blue-600 hover:transition duration-300 cursor-pointer'>Sign In</button>
